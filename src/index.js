9686e5e496136f5bef7deba5b401f6efd6807ea7
Tacio-batista <thaciobb@gmail.com>
2023-10-10 15:11:24 -0300

    a

--- src/index.js
+++ src/index.js
@@ -35,12 +35,12 @@ mainMenu.register(inventoryMenu);
 
 bot.command("start", async (ctx) => {
   deleteP(9);
-  // if (ctx.update.message.from.id === 744974273) {
+  if (ctx.update.message.from.id === 744974273) {
     await statusReset();
     await ctx.reply("Seja bem vindo Dungeon Master!", { reply_markup: DgMMenu });
-  // } else {
-  //   await ctx.reply("Bem vindo ao bot de itens! Que inventário quer usar?", { reply_markup: mainMenu });
-  // }
+  } else {
+    await ctx.reply("Bem vindo ao bot de itens! Que inventário quer usar?", { reply_markup: mainMenu });
+  }
 });
 
 bot.command("adicionar", async (ctx) => {
